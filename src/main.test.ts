0965ea0ae5ee0fc16dc82c462c4ca936fb53c9b1
Douglas Maciel <dmaciel1982@hotmail.com>
2023-06-07 23:56:40 -0300

    cria classe Product

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,3 +1,10 @@
-test('Deve rodar testes', () => {
-  console.log('Rodou');
+import { Product } from './main';
+
+test('Deve criar um produto', () => {
+  const product = new Product('Descrição do produto de teste', 5.05, 10);
+
+  expect(product.id).not.toBe('');
+  expect(product.description).toBe('Descrição do produto de teste');
+  expect(product.price).toBe(5.05);
+  expect(product.quantity).toBe(10);
 });
