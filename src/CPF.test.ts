9d3604df411af3a34eeea1c48bb39de3c0607d22
Douglas Maciel <dmaciel1982@hotmail.com>
2023-06-09 22:01:34 -0300

    implementa verificação do formato do cpf

--- src/CPF.test.ts
+++ src/CPF.test.ts
@@ -7,4 +7,9 @@ test('Deve criar um objeto CPF', () => {
 
 test('Deve lançar uma exceção caso valor do cpf seja inválido', () => {
   expect(() => new CPF('cpf-invalido')).toThrow(InvalidCPF);
+  expect(() => new CPF('659.634.00406')).toThrow(InvalidCPF);
+  expect(() => new CPF('659634.004-06')).toThrow(InvalidCPF);
+  expect(() => new CPF('659.634.004-16')).toThrow(InvalidCPF);
+  expect(() => new CPF('659.634.004-09')).toThrow(InvalidCPF);
+  expect(() => new CPF('111.111.111-11')).toThrow(InvalidCPF);
 });
--- src/CPF.ts
+++ src/CPF.ts
@@ -2,18 +2,60 @@ export class CPF {
   #value: string;
 
   constructor(cpf: string) {
-    this.validate(cpf);
     this.#value = cpf;
+    this.validate();
   }
 
-  private validate(cpf: string) {
-    if (!cpf.match(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)) {
+  private validate() {
+    if (this.#value.search(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/) === -1)
       throw new InvalidCPF();
+    if (
+      this.numericValue
+        .split('')
+        .every((el) => el === this.numericValue.charAt(0))
+    )
+      throw new InvalidCPF();
+    if (
+      parseInt(this.#value.charAt(12)) !==
+      this.calculateFirstVerificationDigit()
+    )
+      throw new InvalidCPF();
+    if (
+      parseInt(this.#value.charAt(13)) !==
+      this.calculateSecondVerificationDigit()
+    )
+      throw new InvalidCPF();
+  }
+
+  private calculateFirstVerificationDigit(): number {
+    const numericCPF = this.numericValue;
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+      sum += (10 - i) * parseInt(numericCPF.charAt(i));
     }
+    const remainder = sum % 11;
+    if (remainder < 2) return 0;
+    else return 11 - remainder;
+  }
+
+  private calculateSecondVerificationDigit(): number {
+    const numericCPF = this.numericValue;
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+      sum += (11 - i) * parseInt(numericCPF.charAt(i));
+    }
+    sum += 2 * this.calculateFirstVerificationDigit();
+    const remainder = sum % 11;
+    if (remainder < 2) return 0;
+    else return 11 - remainder;
   }
 
   get value() {
     return this.#value;
+  }
+
+  get numericValue() {
+    return this.#value.replace(/\D/g, '');
   }
 }
 
