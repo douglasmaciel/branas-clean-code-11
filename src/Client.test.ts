ca3e7cbe128aadeb781b68d647f372fdde953595
Douglas Maciel <dmaciel1982@hotmail.com>
2023-06-10 09:34:35 -0300

    remove id do Client (o cpf já é um identificador)

--- src/Client.test.ts
+++ src/Client.test.ts
@@ -1,11 +1,10 @@
 import { Client } from './Client';
 
 test('Deve criar um cliente', () => {
-  const client = new Client('id-client', '659.634.004-06');
-  expect(client.id).toBe('id-client');
+  const client = new Client('659.634.004-06');
   expect(client.cpf).toBe('659.634.004-06');
 });
 
 test('Deve lançar uma exceção caso CPF seja inválido', () => {
-  expect(() => new Client('id-client', 'cpf-invalido')).toThrow();
+  expect(() => new Client('cpf-invalido')).toThrow();
 });
--- src/Client.ts
+++ src/Client.ts
@@ -1,16 +1,10 @@
 import { CPF } from './CPF';
 
 export class Client {
-  #id: string;
   #cpf: CPF;
 
-  constructor(id: string, cpf: string) {
-    this.#id = id;
+  constructor(cpf: string) {
     this.#cpf = new CPF(cpf);
-  }
-
-  get id() {
-    return this.#id;
   }
 
   get cpf() {
