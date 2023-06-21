a0e0adccbd6f4487667c758822b8c3699738ca52
Douglas Maciel <dmaciel1982@hotmail.com>
2023-06-21 14:27:00 -0300

    adiciona dimensão ao produto

--- src/domain/entity/Product.test.ts
+++ src/domain/entity/Product.test.ts
@@ -1,13 +1,18 @@
+import { Dimension } from '../valueObject/Dimension';
 import { Product } from './Product';
 
 test('Deve criar um produto', () => {
+  const dimension = new Dimension(20, 15, 10, 1);
   const product = new Product(
     'novo-id-do-produto',
     'Descrição do produto de teste',
-    5.05
+    5.05,
+    dimension
   );
 
   expect(product.id).toBe('novo-id-do-produto');
   expect(product.description).toBe('Descrição do produto de teste');
   expect(product.price).toBe(5.05);
+  expect(product.volume).toBe(dimension.volume);
+  expect(product.weight).toBe(dimension.weight);
 });
--- src/domain/entity/Product.ts
+++ src/domain/entity/Product.ts
@@ -1,12 +1,21 @@
+import { Dimension } from '../valueObject/Dimension';
+
 export class Product {
   #id: string;
   #description: string;
   #price: number;
+  #dimension: Dimension;
 
-  constructor(id: string, description: string, price: number) {
+  constructor(
+    id: string,
+    description: string,
+    price: number,
+    dimension: Dimension
+  ) {
     this.#id = id;
     this.#description = description;
     this.#price = price;
+    this.#dimension = dimension;
   }
 
   get id() {
@@ -19,5 +28,13 @@ export class Product {
 
   get price() {
     return this.#price;
+  }
+
+  get volume() {
+    return this.#dimension.volume;
+  }
+
+  get weight() {
+    return this.#dimension.weight;
   }
 }
