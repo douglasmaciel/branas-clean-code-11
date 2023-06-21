838b1af7c4c706378bf3b22617abe1c1ddb519df
Douglas Maciel <dmaciel1982@hotmail.com>
2023-06-21 16:41:57 -0300

    corrige teste

--- src/domain/entity/Order.test.ts
+++ src/domain/entity/Order.test.ts
@@ -3,6 +3,7 @@ import { Client } from './Client';
 import { Coupon } from './Coupon';
 import { DuplicateProduct, Order } from './Order';
 import { Product } from './Product';
+import { Dimension } from '../valueObject/Dimension';
 
 let client: Client;
 let order: Order;
@@ -11,9 +12,14 @@ const products: Product[] = [];
 beforeEach(() => {
   client = new Client('659.634.004-06');
   order = new Order('id-order', client);
-  products.push(new Product('id-1', 'Descrição 1', 5));
-  products.push(new Product('id-2', 'Descrição 2', 7));
-  products.push(new Product('id-3', 'Descrição 3', 3));
+  const [dimension1, dimension2, dimension3] = [
+    new Dimension(1, 2, 3, 1),
+    new Dimension(4, 5, 6, 7),
+    new Dimension(8, 9, 10, 11),
+  ];
+  products.push(new Product('id-1', 'Descrição 1', 5, dimension1));
+  products.push(new Product('id-2', 'Descrição 2', 7, dimension2));
+  products.push(new Product('id-3', 'Descrição 3', 3, dimension3));
 });
 
 test('Deve criar um pedido com 3 produtos e calcular o valor total', () => {
@@ -56,8 +62,9 @@ test('Deve criar um pedido com 3 produtos, associar um cupom de desconto expirad
 });
 
 test('Deve lançar exceção caso seja adicionado a um pedido um produto já existente', () => {
-  const product1 = new Product('id-1', 'Descrição 1', 5);
-  const otherProduct1 = new Product('id-1', 'Descrição 1', 1);
+  const dimension = new Dimension(1, 2, 3, 4);
+  const product1 = new Product('id-1', 'Descrição 1', 5, dimension);
+  const otherProduct1 = new Product('id-1', 'Descrição 1', 1, dimension);
   expect(() =>
     order.addProduct(product1, 1).addProduct(otherProduct1, 2)
   ).toThrow(DuplicateProduct);
